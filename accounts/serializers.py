cd94f364e1d2948c15b643871b8a5744dccf56af
Akirakii <akiraki.awa@gmail.com>
2021-12-22 09:36:25 -0500

    added password validators on user registration

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,4 +1,6 @@
-from rest_framework.serializers import ModelSerializer
+import django.contrib.auth.password_validation as validators
+from django.core import exceptions
+from rest_framework.serializers import ModelSerializer, ValidationError
 
 from accounts.models import User
 
@@ -10,6 +12,28 @@ class UserSerializer(ModelSerializer):
         extra_kwargs = {
             'password': {'write_only': True},
         }
+
+    def validate(self, data):
+        # here data has all the fields which have validated values
+        # so we can create a User instance out of it
+        user = User(**data)
+
+        # get the password from the data
+        password = data.get('password')
+
+        errors = dict()
+        try:
+            # validate the password and catch the exception
+            validators.validate_password(password=password, user=User)
+
+        # the exception raised here is different than serializers.ValidationError
+        except exceptions.ValidationError as e:
+            errors['password'] = list(e.messages)
+
+        if errors:
+            raise ValidationError(errors)
+
+        return super(UserSerializer, self).validate(data)
 
     def create(self, validated_data):
         user = User(
